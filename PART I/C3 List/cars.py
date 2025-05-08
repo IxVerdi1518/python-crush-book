a3755067fe228053cb5d2c5dd33de4628a0dec37
Esteban Flores <estebanfe400@gmail.com>
2025-05-07 23:09:57 -0500

    feat: :sparkles: Cap 3 done

---
+++ PART I/C3 List/cars.py
@@ -0,0 +1,10 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+
+cars.sort(reverse=True)
+print(cars)
+
+cars.reverse()
+print(len(cars))
+
---
+++ PART I/C3 List/seeing_world.py
@@ -0,0 +1,16 @@
+locations=['New York', 'Ecuador', 'Brazil', 'Colombia', 'Canada']
+print(locations)
+print(sorted(locations))
+print(sorted(locations,reverse=True))
+print(locations)
+locations.reverse()
+print(locations)
+locations.reverse()
+print(locations)
+locations.sort()
+print(locations)
+locations.sort(reverse=True)
+print(locations)
+
+guest_list=['samantha', 'glamis', 'wilmer']
+print(len(guest_list))
